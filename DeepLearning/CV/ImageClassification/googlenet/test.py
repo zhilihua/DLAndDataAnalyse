955a5137c92d4229556f27e7dea19af29e865599
hua <771713194@qq.com>
2020-05-22 10:44:47 +0800

    googlenet测试代码

---
+++ DeepLearning/CV/ImageClassification/googlenet/test.py
@@ -0,0 +1,49 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from DeepLearning.CV.tools.ClassAug.readDir import PreDataForVGG
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def predict(model, img, target_size):
+    if img.size != target_size:
+        img = img.resize(target_size)
+
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = PreDataForVGG(x)
+    preds = model.predict(x)
+    return preds[0]
+
+def plot_preds(image, preds, p):
+    index = np.argmax(preds)
+
+    labels = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+
+    plt.imshow(image)
+    plt.axis('off')
+    plt.title("come from %s==>" %p+labels[index]+':'+'%s'%preds[index])
+    plt.show()
+
+if __name__ == '__main__':
+    print("这个是googlenet")
+    import os
+    target_size = (224, 224)
+
+    imagesPath = []
+    image_path = '../../../../datasets/testdatas/flowers'
+    for root, dirs, files in os.walk(image_path):
+        for file in files:
+            path = os.path.join(image_path, file)
+            imagesPath.append(path)
+
+    path_model = 'saveWeights/googlenetModel.h5'   #加载自己学习的googlenet模型
+    # path_model = 'saveWeights/vgg11Model.h5'    #加载自己学习的vgg16迁移模型
+    model = load_model(path_model)
+
+    for p in imagesPath:
+        img = Image.open(p)
+        preds = predict(model, img, target_size)
+        #print(preds)
+        p = p.split('\\')[-1]
+        plot_preds(img, preds, p)
